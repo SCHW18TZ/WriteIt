9dd40ac857418f7de042d0dc3394c0555a706fea
Schwitz <schwitz6969@gmail.com>
2023-03-05 17:19:25 +0530

    Google Login works

--- vite-project/src/Pages/Login.jsx
+++ vite-project/src/Pages/Login.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import GoogleLogin from "../lib/GoogleLogin";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="LoginPage">
+      <GoogleLogin />
+    </div>
+  );
 };
-
 export default Login;
